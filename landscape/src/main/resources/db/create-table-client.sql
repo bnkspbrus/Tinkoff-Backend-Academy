3cfcf71732b9287562dda05c1593c34139a63f46
Nikita Barsukov <bnkspbrus@gmail.com>
2023-03-18 15:03:21 +0300

    adjust table

--- landscape/src/main/resources/db/create-table-client.sql
+++ landscape/src/main/resources/db/create-table-client.sql
@@ -7,12 +7,12 @@ CREATE TYPE client_type AS ENUM('handyman', 'rancher');
 
 CREATE TABLE client
 (
-    id           UUID DEFAULT uuid_generate_v4(),
-    clientType   client_type,
-    login        VARCHAR(255),
-    email        VARCHAR(255),
-    phone        VARCHAR(255),
-    creationDate DATE,
-    updatingTime DATE,
+    id         UUID DEFAULT uuid_generate_v4(),
+    clientType client_type,
+    login      VARCHAR(255),
+    email      VARCHAR(255),
+    phone      VARCHAR(255),
+    creation   timestamp without time zone,
+    updating   timestamp without time zone,
     PRIMARY KEY (id)
 );
