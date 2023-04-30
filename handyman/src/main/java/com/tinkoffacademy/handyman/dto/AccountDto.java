e1da5d60bcce883252df78872d5f8afd60b6572c
Nikita Barsukov <bnkspbrus@gmail.com>
2023-05-01 00:59:07 +0300

    fix

--- handyman/src/main/java/com/tinkoffacademy/handyman/dto/AccountDto.java
+++ handyman/src/main/java/com/tinkoffacademy/handyman/dto/AccountDto.java
@@ -1,10 +1,29 @@
 package com.tinkoffacademy.handyman.dto;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
+import com.fasterxml.jackson.annotation.*;
 
+import java.util.List;
+
+/**
+ * DTO for Account
+ * <p>
+ * This class is used to transfer data from the database to the client and vice versa.
+ * It is used in the {@link com.tinkoffacademy.handyman.controller.AccountController}.
+ * </p>
+ *
+ * @param id        - id of the account. Ignored by Jackson if null while serializing. Ignored while deserializing.
+ * @param login     - login of the account
+ * @param email     - email of the account
+ * @param phone     - phone of the account
+ * @param latitude  - latitude of the account
+ * @param longitude - longitude of the account
+ * @param skills    - skills of the account
+ * @see com.tinkoffacademy.handyman.model.Account
+ * @see com.tinkoffacademy.handyman.controller.AccountController
+ */
+@JsonIgnoreProperties(value = {"id"}, allowGetters = true)
 public record AccountDto(
+        @JsonInclude(JsonInclude.Include.NON_NULL) String id,
         String login,
         String email,
         String phone,
--- handyman/src/main/java/com/tinkoffacademy/handyman/service/AccountService.java
+++ handyman/src/main/java/com/tinkoffacademy/handyman/service/AccountService.java
@@ -3,9 +3,9 @@ package com.tinkoffacademy.handyman.service;
 import com.tinkoffacademy.handyman.dto.AccountDto;
 import com.tinkoffacademy.handyman.model.Account;
 import com.tinkoffacademy.handyman.repository.AccountRepository;
-import com.tinkoffacademy.handyman.view.AccountExt;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
@@ -20,24 +20,26 @@ import java.util.stream.Collectors;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Service
-@RequiredArgsConstructor
 public class AccountService {
-
     private final AccountRepository accountRepository;
 
-    @GrpcClient("landscape")
-    private AccountServiceBlockingStub landscapeStub;
+    private final AccountServiceBlockingStub landscapeStub;
 
-    public AccountExt findById(String id) {
+    public AccountService(AccountRepository accountRepository, @GrpcClient("landscape") AccountServiceBlockingStub landscapeStub) {
+        this.accountRepository = accountRepository;
+        this.landscapeStub = landscapeStub;
+    }
+
+    public AccountDto findById(String id) {
         Account account = accountRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Handyman account with id " + id + " not found"));
 
-        return toAccountExt(account);
+        return toAccountDto(account);
     }
 
-    public List<AccountExt> findAll() {
+    public List<AccountDto> findAll() {
         return accountRepository.findAll().stream()
-                .map(this::toAccountExt)
+                .map(this::toAccountDto)
                 .collect(Collectors.toList());
     }
 
@@ -70,12 +72,21 @@ public class AccountService {
         accountRepository.deleteById(id);
     }
 
-    private AccountExt toAccountExt(Account account) {
+    /**
+     * Converts Account to AccountDto
+     *
+     * @param account Account
+     * @return AccountDto
+     */
+    private AccountDto toAccountDto(Account account) {
         UUIDProto uuid = UUIDProto.newBuilder()
                 .setValue(account.getParentUUID())
                 .build();
         AccountCredProto accountCredProto = landscapeStub.findById(uuid);
-        return new AccountExt(
+        // map account and accountCredProto to AccountDto
+        // hint: use id, skills, latitude, longitude from account
+        // hint: use login, email, phone from accountCredProto
+        return new AccountDto(
                 account.getId(),
                 accountCredProto.getLogin(),
                 accountCredProto.getEmail(),
